101aaacdb35a325b7014724aad79427ca9efbf21
João Fernandes <joao.fernandes.fra@gmail.com>
2025-06-12 16:25:41 -0300

    [KZ-000] Atualizado tema e aplicando algumas alterações no layou

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
         "./src/**/*.{js,ts}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'zacarin-beige': '#F1E2E0',      // fundo claro
+                'zacarin-lavender': '#6D6173',   // texto, icons (tema escuro)
+                'zacarin-plum': '#6D6173',       // fundo escuro
+                'zacarin-mauve': '#F1E2E0',      // texto claro no fundo escuro
+            },
+            fontFamily: {
+                'display': ['"Merriweather"', 'serif'],
+                'heading': ['"Montserrat"', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 }
